fc70e69a3964026e1ae18b0e80f1a3f646058a0e
muhammadashfaq <ashfaq441997@gmail.com>
2019-06-09 06:01:29 -0700

    Webview in react native

---
+++ App.js
@@ -0,0 +1,32 @@
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { WebView } from 'react-native-webview';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <WebView source={{ uri: 'http://aworka.com' }} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
